bb1cb92ea89f2d623d2b8c794da6faf6591e2874
LaszloP <7979773+lpusok@users.noreply.github.com>
2025-04-25 18:59:31 +0200

    Do not retry if context cancelled

--- download.go
+++ download.go
@@ -308,6 +308,7 @@ func (d *Download) IsRangeable() bool {
 
 // Download chunks
 func (d *Download) dl(dest io.WriterAt, errC chan error) {
+	d.Concurrency = 1
 	var (
 		// Wait group.
 		wg sync.WaitGroup
@@ -315,6 +316,8 @@ func (d *Download) dl(dest io.WriterAt, errC chan error) {
 		// Concurrency limit.
 		max = make(chan int, d.Concurrency)
 	)
+
+	d.Logger.Debugf("Downloading %d chunks, %d concurrency", len(d.chunks), d.Concurrency)
 
 	var stats chunkStatistics
 	for i := 0; i < len(d.chunks); i++ {
@@ -330,7 +333,7 @@ func (d *Download) dl(dest io.WriterAt, errC chan error) {
 			// - in case of an interrupt and re-download, it will resume from the last position
 			offsetWriter := &OffsetWriter{dest, int64(d.chunks[i].Start)}
 
-			err := retry.Times(uint(d.MaxRetryPerChunk)).Try(func(attempt uint) error {
+			err := retry.Times(uint(d.MaxRetryPerChunk)).TryWithAbort(func(attempt uint) (error, bool) {
 				log := func(msg string, args ...interface{}) {
 					if d.Logger == nil {
 						return
@@ -367,13 +370,18 @@ func (d *Download) dl(dest io.WriterAt, errC chan error) {
 				}()
 
 				if err := d.DownloadChunk(chunkCtx, offsetWriter, d.chunks[i].End); err != nil {
-					return err
+					if d.ctx.Err() != nil {
+						// Do not retry if context cancelled or deadline exceeded
+						log("timouted, aborting: %s", err)
+						return err, true
+					}
+					return err, false
 				}
 
 				took := time.Since(start)
 				stats.update(took)
 				log("finished chunk download, took %s", took)
-				return nil
+				return nil, false
 			})
 			if err != nil {
 				errC <- err
